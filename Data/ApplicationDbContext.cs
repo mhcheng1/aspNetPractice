68f17b3fdc0c478a05d7259a5e888a63a3fc8ebd
Meng-Hsin Cheng <david999qq@gmail.com>
2021-06-05 16:41:18 -0400

    First Commit

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using aspNetPractice.Models;
 
 namespace aspNetPractice.Data
 {
@@ -12,5 +13,6 @@ namespace aspNetPractice.Data
             : base(options)
         {
         }
+        public DbSet<aspNetPractice.Models.Joke> Joke { get; set; }
     }
 }
--- Data/Migrations/20210605192944_initialSetup.Designer.cs
+++ Data/Migrations/20210605192944_initialSetup.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using aspNetPractice.Data;
 
 namespace aspNetPractice.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210605192944_initialSetup")]
+    partial class initialSetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
